23c71e47049484e9bc88ba920044d3e2f945dd8d
yoga <yogamadness@gmail.com>
2018-11-02 09:44:15 +0700

    Initial Commit

---
+++ CollectionView2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView2
+//
+//  Created by Yoga Pratama on 02/11/18.
+//  Copyright © 2018 YPA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
